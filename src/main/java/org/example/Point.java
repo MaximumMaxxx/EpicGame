48fa1c90faa07d220723194767f515c2810e76d7
Matt <2024256@apps.nsd.org>
2023-05-23 14:57:50 -0700

    point

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+
+    }
+    public double getx(){
+        return this.x;
+    }
+    public double gety(){
+        return this.y;
+    }
+}
