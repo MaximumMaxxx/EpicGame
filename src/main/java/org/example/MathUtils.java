68e7853556decab06fa3bff437e25829582e1d85
Max <maxwellwindland@gmail.com>
2023-05-24 21:16:06 -0700

    This will fix the tests

--- src/main/java/org/example/MathUtils.java
+++ src/main/java/org/example/MathUtils.java
@@ -12,10 +12,10 @@ public class MathUtils {
         // Thanks Issac for suggestion atan2 instead of normal atan
         double angle = Math.toDegrees(Math.atan2((y2-y1),(x2-x1)));
 
-//        // Atan2 sometimes returns negative angles, this makes sure it is always positive, not technically required but makes the numbers look better
-//        if (angle < 0) {
-//            angle += 360;
-//        }
+        // Atan2 sometimes returns negative angles, this makes sure it is always positive, not technically required but makes the numbers look better
+        if (angle < 0) {
+            angle += 360;
+        }
         return angle;
     }
 
