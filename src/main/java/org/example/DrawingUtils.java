715d046ca1f85fd109e6fd803a89909b3ae20d27
MaximumMaxxx <maxwellwindland@gmail.com>
2023-05-26 14:19:31 -0700

    Commented code

--- src/main/java/org/example/DrawingUtils.java
+++ src/main/java/org/example/DrawingUtils.java
@@ -2,6 +2,13 @@ package org.example;
 import java.awt.*;
 
 public class DrawingUtils {
+    /**
+     * Draws a line along an angle - by Max
+     * @param g A Graphics Object
+     * @param angle The angle to draw the line in degrees
+     * @param length The length of the line
+     * @param location The location of the line
+     */
     public static void lineAlongAngle(Graphics g, double angle, double length, Point location) {
         // Trig crap
         int xOffset = (int) (MathUtils.degreeCos(angle)*length);
@@ -9,5 +16,15 @@ public class DrawingUtils {
 
 
         g.drawLine((int) location.getx(), (int) location.gety(), (int) (location.getx()+xOffset), (int) (location.gety()+yOffset));
+    }
+
+    /**
+     * "Flip" the screen by drawing a white rectangle over the whole screen. Sets color to White - by Max
+     * @param p The DrawingPanel
+     * @param g The Graphics Object
+     */
+    public static void screenFlip(DrawingPanel p, Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0,p.getWidth(),p.getHeight());
     }
 }
