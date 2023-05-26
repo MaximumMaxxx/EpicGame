61e0a9013eae413c9e7a8cfd440ba257a00ae522
MaximumMaxxx <maxwellwindland@gmail.com>
2023-05-26 14:19:48 -0700

    Moved stuff around a bit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyListener;
 import java.util.*;
 
 public class Main {
+    private static final int FRAMERATE = 240;
     public static void main(String[] args) {
         DrawingPanel panel = new DrawingPanel();
         PanelInput input = new PanelInput(panel);
@@ -13,7 +14,6 @@ public class Main {
         boolean running = true;
 
         Player character = new Player((double) panel.getWidth() / 2 - 5, (double) panel.getHeight() / 2 - 5, 0, g);
-        character.render();
 
 
         int x = panel.getWidth() / 2;
@@ -22,19 +22,24 @@ public class Main {
         int mousex;
         int mousey;
         while (running) {
+            // LEAVE THIS HERE
+            DrawingUtils.screenFlip(panel, g);
+            g.setColor(Color.green);
+
             mousex = input.getMouseLoc().x;
             mousey = input.getMouseLoc().y;
 
+            character.render();
 
-            int width = 10;
-            g.drawRect(x-width/2, y-width/2, 10,10);
+//            int width = 10;
+//            g.drawRect(x-width/2, y-width/2, 10,10);
 
-            double angle = MathUtils.angleToPoint(x,y,mousex,mousey);
-            DrawingUtils.lineAlongAngle(g,angle,100,new Point(x,y));
+//            double angle = MathUtils.angleToPoint(x,y,mousex,mousey);
+//            DrawingUtils.lineAlongAngle(g,angle,100,new Point(x,y));
 
 
 
-            panel.sleep(10);
+            panel.sleep(1000/FRAMERATE);
 
             if (DrawingPanel.instances == 0) {
                 running = false;
