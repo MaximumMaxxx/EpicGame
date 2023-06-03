df02c8ca03eae6c7db51c7521213c4e0cad86520
MaximumMaxxx <maxwellwindland@gmail.com>
2023-06-03 00:39:02 -0700

    Improved Hud rendering

--- src/main/java/org/amorgugus/Main.java
+++ src/main/java/org/amorgugus/Main.java
@@ -3,26 +3,51 @@ package org.amorgugus;
 import java.awt.*;
 
 public class Main {
-    private static final int FRAMERATE = 240;
+    private static final int FRAMERATE = 60;
     public static void main(String[] args) {
-        DrawingPanel panel = new DrawingPanel();
+        DrawingPanel panel = new DrawingPanel(1280, 720);
         PanelInput input = new PanelInput(panel);
         Graphics g = panel.getGraphics();
         boolean running = true;
 
         Player character = new Player((double) panel.getWidth() / 2 - 5, (double) panel.getHeight() / 2 - 5, 0, g);
+        Line tempWall = new Line(new Point(66,58), new Point(228,19));
 
         int mousex;
         int mousey;
+        HUD hud = new HUD(g, panel);
         while (running) {
+
             // LEAVE THIS HERE
             DrawingUtils.screenFlip(panel, g);
+
+            // Variables to render on hud
+            String[] hudVars = new String[] {
+                "Intersect point" + character.getLine().getIntersect(tempWall), // Wall intersection
+                "P -> Wall Intersect " + character.getLine().doesIntersect(tempWall),
+                    "Wall -> P Intersect " + tempWall.doesIntersect(character.getLine()),
+                "Player Position " + character.getPoint()
+            };
+            // End variables
+
+            hud.render(hudVars);
             g.setColor(Color.green);
 
             mousex = input.getMouseLoc().x;
             mousey = input.getMouseLoc().y;
 
+            character.lookAt(new Point(mousex, mousey));
+
             character.render();
+
+            g.setColor(Color.orange);
+            tempWall.draw(g);
+
+            if (tempWall.doesIntersect(character.getLine())) {
+                System.out.println("They intersect");
+            }
+
+
 
             if (input.keyDown('w')) {
                 character.move(1,0);
